0976c521f86029c0c41815e6249a3fb013c120d8
Federico Trani <federicotrani@gmail.com>
2022-09-29 22:41:52 -0300

    first commit

---
+++ Program copy.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace DemoConsola01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string opcion = "";
+          int resultadoMultiplicacion=0;
+          int otraVariable = 0;
+
+            Console.ForegroundColor = ConsoleColor.Green;      
+
+            while(opcion != "x"){
+              Console.Clear();
+              System.Console.WriteLine("MENU de USUARIO");
+              System.Console.WriteLine("S - OPERACION SUMA");
+              System.Console.WriteLine("R - OPERACION RESTA");
+              System.Console.WriteLine("M - OPERACION MULTIPLICACION");
+              System.Console.WriteLine("D - OPERACION DIVISION");
+              System.Console.WriteLine("X - TERMINAR PROGRAMA");
+
+              opcion = Console.ReadLine();
+
+              switch(opcion){                
+                
+                case "s" or "S":
+                  operacionSumar(18, 23);
+                  break;
+                case "r" or "R":
+                  System.Console.WriteLine("Seleccionamos RESTA");
+                  Console.ReadKey();
+                  break;
+                case "m" or "M":
+                  resultadoMultiplicacion = 56 + otraVariable + operacionMutiplicar(55, 89);
+                  break;
+                case "d" or "D":
+                  System.Console.WriteLine("Seleccionamos DIVISION");
+                  Console.ReadKey();
+                  break;
+                default:
+                  System.Console.WriteLine("Opción NO VALIDA");
+                  Console.ReadKey();
+                  break;
+              }             
+            }
+
+            System.Console.WriteLine("Salimos del programa...");
+            Console.ReadKey();
+        }
+
+        static void operacionSumar(int valorUno, int valorDos){
+          int resultado = 0;
+          resultado = valorUno + valorDos;
+          Console.Clear();
+          System.Console.WriteLine("Resultado {0}", resultado);
+          Console.ReadKey();
+        }
+    
+        static int operacionMutiplicar(int numPrimero, int numSegundo){
+
+          int resultadoMultiplicacion = numPrimero *numSegundo;
+
+          return resultadoMultiplicacion;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace conversion_dolar_y_euro
+{
+    class Program
+    {        
+        static void menu(){
+            int  op, cotizacion_dolar, cotizacion_euro, dolar, euro, peso_argentino;
+            string op2, espacio_salida;
+            
+            Console.WriteLine(" 1. Conversion de Euro a Pesos Argentino");
+            Console.WriteLine(" 2. Conversion de Dolar a Pesos Argentino");
+            Console.WriteLine(" 3. Salir del programa");
+
+            Console.WriteLine("elige una opcion");
+            op = Convert.ToInt32(Console.ReadLine());
+            
+            
+            switch (op)
+            {
+                case 1:
+                    /* cotizacion_euro = 143;
+                    Console.WriteLine("Cuantos Euros desea convertir a Pesos argentinos");
+                    euro = Convert.ToInt32(Console.ReadLine());
+                    peso_argentino = euro * cotizacion_euro;
+                    Console.Clear();
+                    Console.WriteLine("Los {0} (Euros) equivale a $ {1} (Pesos Argentinos) ", euro, peso_argentino); */
+                    cotizacion_euro2();
+                    
+                    Console.Clear();
+                    Console.WriteLine("Los {0} (Euros) equivale a $ {1} (Pesos Argentinos) ", euro, peso_argentino);
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine("desea continuar:");
+                    Console.WriteLine(" presione S-> si / N ->no");
+                    op2 = Console.ReadLine();
+                    if((op2=="s")||(op2=="S")){
+                    Console.Clear();
+                    menu ();
+                    op = Convert.ToInt32(Console.ReadLine());
+                    }else{
+                    
+                        Console.WriteLine("Muchas gracias, por usar el programa");
+                    }
+                    
+                    break;
+                case 2:
+                    cotizacion_dolar = 287;
+                    Console.WriteLine("Cuantos Dolares desea convertir a Pesos argentinos");
+                    dolar = Convert.ToInt32(Console.ReadLine());
+                    peso_argentino = dolar * cotizacion_dolar;
+                    Console.WriteLine("Los {0} (Dolares) equivale a $ {1} (Pesos Argentinos) ", dolar, peso_argentino);
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine("desea continuar:");
+                    Console.WriteLine(" presione S-> si / N ->no");
+                    op2 = Console.ReadLine();
+                    if((op2=="s")||(op2=="S")){
+                    Console.Clear();
+                    menu ();
+                    op = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else {
+                        Console.WriteLine("Muchas gracias, por usar el programa");
+                    }
+                    
+                    break;
+                case 3:
+                    Console.WriteLine("Muchas gracias, por usar el programa");
+                    espacio_salida=Console.ReadLine();
+                    
+                break;
+                default:
+                    Console.WriteLine("No corresponde a ninguna operacion. Seleccione una operacion valida");
+                    Console.WriteLine("Desea volver al inicio o salir");
+                    Console.WriteLine("Presione I -> inicio / S -> salir ");
+                    op2= Console.ReadLine();
+                    if ((op2=="i")||(op2=="I"))
+                    {
+                        Console.Clear();
+                        menu();
+                        op = Convert.ToInt32(Console.ReadLine());
+                    }else
+                    {
+                        Console.WriteLine("Muchas gracias, por usar el programa");
+                    }
+                break;
+                
+            }
+            
+            
+        }
+
+        static int cotizacion_euro2(){
+                    int cotizacion_euro = 143,euro,peso_argentino;
+                    Console.WriteLine("Cuantos Euros desea convertir a Pesos argentinos");
+                    euro = Convert.ToInt32(Console.ReadLine());
+                    return peso_argentino = euro * cotizacion_euro;
+                    
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.Clear();
+            Console.WriteLine("Bienvenido a programa cotizacion:");
+            menu();
+            
+        }
+    }
+}
